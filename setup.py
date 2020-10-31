b4b135b56ffa47355aaf6c49edb723b1b78f43ca
Pr0stoVanya <71609416+Pr0stoVanya@users.noreply.github.com>
2020-10-31 17:54:12 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ournash',
+      version='1.0',
+      description='OurNash',
+      url='https://bitbucket.org/BorisovAl/matrix-game',
+      author='IoTeam',
+      license='MIT',
+      packages=['ournash'],
+      zip_safe=False)
